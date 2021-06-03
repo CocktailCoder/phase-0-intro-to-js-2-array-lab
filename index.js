b285789f57e01889380e2fb70bdf3f7e30520001
cocktailcoder <iokreddit@protonmail.com>
2021-06-02 23:02:55 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+   cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+
+function appendCat(name){
+    cats.slice(cats);
+    return [...cats, name];
+}
+
+function prependCat(name){
+    cats.slice(cats);
+    return [name, ...cats];
+}
+
+function removeLastCat(){
+    cats.slice(cats);
+    return [...cats.slice(0, 2)];
+}
+
+function removeFirstCat(){
+    cats.slice(cats);
+    return [...cats.slice(1)];
+}
